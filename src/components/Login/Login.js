d91fa3b6fd4d4305e9b74e4f489caf7a485f7a2a
Mehedi390 <mh0816150@gmail.com>
2022-08-02 09:10:56 -0700

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,28 +1,28 @@
 import React from "react";
-
+import loginimg from "../../images/login.png";
 import { NavLink, useNavigate } from "react-router-dom";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
-import { unstable_HistoryRouter, useLocation } from "react-router-dom";
-import UseFirebase from "../../Hook/UseFirebase";
-import { Box, CircularProgress, Typography } from "@mui/material";
+
+import { useLocation } from "react-router-dom";
+
 import { useForm } from "react-hook-form";
+
+import PasswordTextField from "../ShareComponents/PasswordTextField/PasswordTextField";
 const Login = () => {
   const location = useLocation();
 
   const {
     register,
-    handleSubmit,
-    watch,
+
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => {
     console.log(data);
   };
   return (
-    <div className="h-screen  flex items-center justify-center flex-col ">
-      <div className="w-11/12	shadow-md md:w-1/3 md:h-3/5 px-3 py-8 rounded-lg">
-        <h2 className="font-bold text-xl mb-2">Login Form</h2>
+    <div className="h-screen flex items-center justify-center flex-col ">
+      <div className="w-11/12	shadow-md md:w-1/3  px-3 py-8 rounded-lg">
+        <img className="img-fluid" src={loginimg} alt="" />
+        <h2 className="font-bold text-xl mb-2">Login</h2>
         <hr className="w-10   border-t-4 rounded-full" />
         <form className="flex flex-col">
           <input
@@ -34,14 +34,11 @@ const Login = () => {
             {...register("email", { required: true })}
           />
           {errors.email && <div>This filed is required</div>}
-          <input
-            className="placeholder-gray-500 bg-transparent border border-gray-300 mt-4 px-4 py-2 rounded-full"
-            placeholder="Password"
-            label="Password"
+          <PasswordTextField
+            register={register}
             name="password"
-            variant="standard"
-            {...register("password", { required: true, minLength: 6 })}
-          />
+          ></PasswordTextField>
+
           {errors.password && <div>password must be 6 length</div>}
           <div>
             <input
@@ -51,20 +48,11 @@ const Login = () => {
             />
           </div>
         </form>
-
-        <Button
-          sx={{
-            px: 3,
-            py: 1.3,
-            color: "gray",
-            borderRadius: 30,
-            border: "1px solid rgba(0, 0, 0,.3)",
-          }}
-          to="/signUp"
-          component={NavLink}
-        >
-          Dont have Account?{" "}
-        </Button>
+        <div className="  flex align-center justify-center ">
+          <NavLink to="/signUp" className="text-center text-primary py-2 ">
+            Don't have Account? <span className="">Let's Create account</span>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,10 @@
 import React from "react";
+
 import { useForm } from "react-hook-form";
 import { NavLink } from "react-router-dom";
+import signImg from "../../images/log.png";
 
+import PasswordTextField from "../ShareComponents/PasswordTextField/PasswordTextField";
 const SignUp = () => {
   const {
     register,
@@ -15,8 +18,9 @@ const SignUp = () => {
   return (
     <div>
       <div className="h-screen  flex items-center justify-center flex-col ">
-        <div className="w-11/12 shadow-md md:w-1/3 md:h-11/12 px-3 py-8 rounded-lg">
-          <h2 className="font-bold text-xl mb-2">SignUp Form</h2>
+        <div className="w-11/12 shadow-md md:w-1/3   px-3  rounded-lg">
+          <img className="img-fluid" src={signImg} alt="" />
+          <h2 className="font-bold text-xl mb-2">SignUp </h2>
           <hr className="w-10 border-t-4 rounded-full" />
           <form className="flex flex-col" onSubmit={onSubmit}>
             <input
@@ -35,24 +39,15 @@ const SignUp = () => {
               {...register("email", { required: true })}
             />
             {errors.email && <div>This filed is required</div>}
-            <input
-              className="placeholder-gray-500 bg-transparent border border-gray-300 mt-4 px-4 py-2 rounded-full"
-              type="password"
-              placeholder="Password must be 6 letter or more"
-              label="Password"
+            <PasswordTextField
+              register={register}
               name="password"
-              variant="standard"
-              {...register("password", { required: true, minLength: 6 })}
-            />
+            ></PasswordTextField>
             {errors.password && <div>password must be 6 length</div>}
-            <input
-              className="placeholder-gray-500 bg-transparent border border-gray-300 mt-4 px-4 py-2 rounded-full"
-              type="password"
-              placeholder="Confirm password"
-              label="Confirm password"
-              variant="standard"
-              {...register("password2", {})}
-            />
+            <PasswordTextField
+              register={register}
+              name="password2"
+            ></PasswordTextField>
             {errors.password2 && <div>{errors.password2.message}</div>}
             <input
               className="my-3 cursor-pointer transition-all hover:shadow-lg text-lg py-2 font-bold px-6 border-gray-300 text-black border  rounded-full"
